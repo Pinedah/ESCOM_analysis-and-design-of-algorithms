2bf1c1e69913281f286cc944291718f05f6f1f6b
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-09-03 11:34:28 -0600

    a

--- ParDeTres_Diagnostico.c
+++ ParDeTres_Diagnostico.c
@@ -7,37 +7,86 @@ Integrantes: Andrea Acevedo Medina, Francisco Pineda Hernandez, Rodrigo Vidal Ra
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
-int generarAleatorio(int menor, int mayor){
-    return rand() % (mayor - menor + 1) + menor;
+int generarAleatorio(int menor, int mayor, int parImpar){
+
+    int random = rand() % (mayor - menor + 1) + menor;
+    if(parImpar == 1){ 
+        if(random % 2 == 0){ // IMPAR
+            if(random == mayor){
+                random -= 1;
+            }
+            if(random == menor){
+                random += 1;
+            }
+            return random;
+        }else{
+            return random;
+        }
+    }else{
+        if(random % 2 != 0){ // PAR
+            if(random == mayor){
+                random -= 1;
+            }
+            if(random == menor){
+                random += 1;
+            }
+            return random;
+        }else{
+            return random;
+        }
+    }
 }
 
 int main(){
 
-    int intMayor = 10, intMenor = 0;
+    srand(time(NULL));
 
-    printf("\nElige un numero entre el 0 y el 500\n");
-    int aleatorio = generarAleatorio(intMenor, intMayor);
+    float promedio = 0;
+    int mejorCaso = 0, peorCaso = 0, iteraciones = 5;
+    for (int i = 0; i < iteraciones; i++){
 
-    int opcion = 2, contador = 0;
-    do{
-        printf("\nTu numero es: %d \n0 -> Si es tu numero \n1 -> Es mayor \n2 -> Es menor\n\n", aleatorio);
-        scanf("%d", &opcion);
-        if (opcion == 1){
-            intMenor = aleatorio + 1;
-            aleatorio = generarAleatorio(intMenor, intMayor);
-            contador++;
-        }else if(opcion == 2){
-            intMayor = aleatorio - 1;
-            aleatorio = generarAleatorio(intMenor, intMayor);
-            contador++;
-        }
         
-    }while (opcion != 0);
+        int intMayor = 100, intMenor = 0, parImpar = 0;
+        printf("\nIteracion: %d \nElige un numero entre el 0 y el %d\n", i + 1,  intMayor);
+
+        printf("Indica si es impar(1) par(2): ");
+        scanf("%d", &parImpar);
+
+        int aleatorio = generarAleatorio(intMenor, intMayor, parImpar);
+
+        int opcion = 2, contador = 0;
+        do{
+            printf("\nTu numero es: %d \n0 -> Si es tu numero \n1 -> Es mayor \n2 -> Es menor\n\n", aleatorio);
+            scanf("%d", &opcion);
+            if (opcion == 1){
+                intMenor = aleatorio + 1;
+                aleatorio = generarAleatorio(intMenor, intMayor, parImpar);
+                contador++;
+            }else if(opcion == 2){
+                intMayor = aleatorio - 1;
+                aleatorio = generarAleatorio(intMenor, intMayor, parImpar);
+                contador++;
+            }
+            
+        }while (opcion != 0);
+        printf("\nEl numero de iteraciones fueron: %d\n\n", contador);
+
+        if(contador > peorCaso){
+            peorCaso = contador;
+        }
+        if(contador < mejorCaso){
+            mejorCaso = contador;
+        }
+
+        promedio += contador;
+    }
     
-    printf("\nEl numero de iteraciones fueron %d\n\n", contador);
+    promedio /= iteraciones;
     
+    printf("\n\nEl peor caso fue: %d \nEl mejor caso fue: %d \nEl promedio fue: %0.3f", peorCaso, mejorCaso, promedio);
     
 
     return 0;
