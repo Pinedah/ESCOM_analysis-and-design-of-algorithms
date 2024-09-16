872acef775bd94d667682063e88d5cbf2f3743ac
Andrea Acevedo <148020397+AndreaAcevedo12@users.noreply.github.com>
2024-09-15 21:05:15 -0600

    Formato bubble-sort.c

--- PrimerParcial/Practica1/bubble-sort.c
+++ PrimerParcial/Practica1/bubble-sort.c
@@ -1,4 +1,4 @@
-//!! PROYECTO ALGORITMOS
+//! Bubble Sort
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,30 +8,28 @@ void llenar(int *arr, int longi);
 void recorrer(int *arr, int longi);
 
 int main() {
-
-
     clock_t start, end;
     srand(time(NULL));
-    
     int longi;
+    
     printf("Ingrese longitud del array: ");
     scanf("%d", &longi);
 
     int *arr = (int*)malloc(longi * sizeof(int));
 
     llenar(arr, longi);
-
     printf("Ordenado usando bubble: \n");
+    //Empezamos el contador de tiempo
     start = clock();
     bubble(arr, longi);
+    //El contador termina
     end = clock();
 
     double time_taken = ((double)end - start) / CLOCKS_PER_SEC;
-
-
+    //Se imprime el arreglo ordenado y el tiempo de ordenamiento
     recorrer(arr, longi);
     printf("\nTiempo: %f", time_taken);
-
+    
     free(arr);
     return 0;
 }
@@ -42,7 +40,7 @@ void llenarAscendente(int *arr, int longi) {
     for(ptr=arr; ptr < &arr[longi]; ptr++) {
         *ptr=i++;
     }
-};
+}
 
 void llenarDesendente(int *arr, int longi) {
     int *ptr;
@@ -50,10 +48,9 @@ void llenarDesendente(int *arr, int longi) {
     for(ptr=arr; ptr < &arr[longi]; ptr++) {
         *ptr=i--;
     }
-};
+}
 
 void llenar(int *arr, int longi) {
-
     FILE *archivo = fopen("numeros10millones.txt", "r");
     if (archivo == NULL){
         printf("No se leyó nada\n");
@@ -61,10 +58,7 @@ void llenar(int *arr, int longi) {
     }
     for(int i; i<longi; i++)
         fscanf(archivo,"%d", &arr[i]);
-};
-
-
-
+}
 
 void recorrer(int *arr, int longi) {
     int *ptr;
@@ -72,10 +66,10 @@ void recorrer(int *arr, int longi) {
         printf("%d ", *ptr);
     }
     printf("\n");
-};
+}
 
+//Función del algoritmo de ordenamiento
 void bubble(int *arr, int longi) {
-    
     int *i, *j;
     for (i = arr + longi; i >= arr; i--){
         for (j = arr; j < (i - 1); j++){
