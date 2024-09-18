75a04f5b921e151a3e7616097385a3f56a477605
Rodrigo Ramirez <ramirez.aguilar.rodrigo.vidal@gmail.com>
2024-09-17 18:12:34 -0600

    Archivos Quicksort y Mergesort, apuntadores

--- PrimerParcial/Practica2/merge-sort.c
+++ PrimerParcial/Practica2/merge-sort.c
@@ -1,72 +1,96 @@
-
 #include <stdio.h>
+#include <time.h>
+#include<stdlib.h>
 
-void mergeSort(int A[], int p, int r);
-void merge(int A[], int p, int q, int r);
+void mergeSort(int *, int , int );
+void merge(int *, int, int , int );
+void generarArchivoOrden(int *, int);
+void llenarArray(int *, int);
 
-int main()
-{
-
-    int arr[] = {5, 4, 2, 1, 8, 3};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
+int main(){
+    int n =10000000;
+    int *arr = malloc(n * sizeof(int));
+    llenarArray(arr, n);
+    clock_t start, end;
+    start = clock();
     mergeSort(arr, 0, n - 1);
-
-    printf("Arreglo ordenado:\n");
-    for(int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+    end = clock();
+    generarArchivoOrden(arr,n);
+    double time_taken = ((double)end - start) / CLOCKS_PER_SEC;
+    printf("Tiempo tomado para ordenar %d numeros: %f segundos\n", n, time_taken);
+    free(arr);
 
     return 0;
 }
 
-void mergeSort(int A[], int p, int r){
+void mergeSort(int *arr, int p, int r){
     int q;
     if (p < r){
         q = p + (r - p) / 2;
 
-        mergeSort(A, p, q);
-        mergeSort(A, q + 1, r);
+        mergeSort(arr, p, q);
+        mergeSort(arr, q + 1, r);
 
-        merge(A, p, q, r);
+        merge(arr, p, q, r);
     }
 }
 
-void merge(int A[], int p, int q, int r){
+void merge(int *arr, int p, int q, int r){
 
     int n1 = q - p + 1;
     int n2 = r - q;
-    int L[n1];
-    int R[n2];
+    int *L = (int *)malloc(n1 * sizeof(int));
+    int *R = (int *)malloc(n2 * sizeof(int));
 
 
     for (int i = 0; i < n1; i++){
-        L[i] = A[p + i];
+        *(L+i) = *(arr + p + i);
     }
     for (int j = 0; j < n2; j++){
-        R[j] = A[q + j + 1];
+        *(R+j) = *(arr + q + j + 1);
     }
 
     int i = 0, j = 0, k = p;
-
     while (i < n1 && j < n2){
-        if (L[i] <= R[j]){
-            A[k] = L[i];
+        if (*(L+i) <= *(R+j)){
+            *(arr+k) = *(L+i);
             i++;
         }else{
-            A[k] = R[j];
+            *(arr+k)= *(R+j);
             j++;
         }
         k++;
     }
 
     while (i < n1){
-        A[k] = L[i];
+        *(arr+k) = *(L+i);
         i++;
         k++;
     }
     while (j < n2){
-        A[k] = R[j];
+        *(arr+k) = *(R+j);
         j++;
         k++;
     }
+    free(L);
+    free(R);
+}
+void llenarArray(int *arr, int longi) {
+    FILE *archivo = fopen("numeros10millones.txt", "r");
+    if (archivo == NULL){
+        printf("No se leyó nada\n");
+        exit(0);
+    }
+    for(int i; i<longi; i++)
+        fscanf(archivo,"%d", &arr[i]);
+
+};
+void generarArchivoOrden(int *arr, int n) {
+    char filename[20];
+    sprintf(filename, "sorted%d.txt", n);
+    FILE *file = fopen(filename, "w");
+    for (int j = 0; j < n; j++) {
+        fprintf(file, "%d\n", arr[j]);
+    }
+    fclose(file);
 }
--- PrimerParcial/Practica2/quick-sort.c
+++ PrimerParcial/Practica2/quick-sort.c
@@ -5,75 +5,69 @@
 void quicksort(int*, int, int);
 void partition(int*, int, int, int*);
 void generarArchivos(int*, int);
-
+void llenarArray(int *, int);
 int main() {
-    int sizes[] = {1000000}; // Define los tamaños de los arreglos a generar
-    for (int i = 0; i < 1; i++) { // Itera sobre cada tamaño
-        int n = sizes[i]; // Toma el tamaño actual
-        int *arr = malloc(n * sizeof(int)); // Reserva memoria para el arreglo
-        generarArchivos(arr, n); // Genera números aleatorios y los escribe en un archivo
-
-        clock_t start, end; // Variables para medir el tiempo
-        start = clock(); // Toma el tiempo antes de ordenar
-        quicksort(arr, 0, n - 1); // Ordena el arreglo
-        end = clock(); // Toma el tiempo después de ordenar
-        double time_taken = ((double)end - start) / CLOCKS_PER_SEC; // Calcula el tiempo que tardó en ordenar
-
-        char filename[20]; // Buffer para el nombre del archivo
-        sprintf(filename, "sorted%d.txt", n); // Genera el nombre del archivo
-        FILE *file = fopen(filename, "w"); // Abre el archivo para escritura
-        for (int j = 0; j < n; j++) { // Itera sobre cada número en el arreglo
-            fprintf(file, "%d\n", arr[j]); // Escribe el número en el archivo
-        }
-        fclose(file); // Cierra el archivo
-
-        printf("Tiempo tomado para ordenar %d números: %f segundos\n", n, time_taken); // Imprime el tiempo que tardó en ordenar
-        free(arr); // Libera la memoria reservada para el arreglo
-    }
-    return 0; // Termina el programa
+        int n =10000000;
+        int *arr = malloc(n * sizeof(int));
+        llenarArray(arr, n);
+        clock_t start, end;
+        start = clock();
+        quicksort(arr, 0, n - 1);
+        end = clock();
+        generarArchivos(arr,n);
+        double time_taken = ((double)end - start) / CLOCKS_PER_SEC;
+        printf("Tiempo tomado para ordenar %d numeros: %f segundos\n", n, time_taken);
+        free(arr);
+    return 0;
 }
 
 void quicksort(int *x, int lb, int ub) {
     if (lb >= ub) {
         return;
-    }// Si el limite inferior del arreglo es mayor o igual que el limite superior el arreglo o subarreglo esta ordenado
-    int pj;
-    partition(x, lb, ub, &pj); //llama a partition que logra que el elemento en la primera posicion del arreglo se coloque en la posicion que le corresponde
-    // es decir que los elementos a su izquierda son menores y a su derecha mayores o iguales que ese elemento
-    quicksort(x, lb, pj - 1);//llamada recursiva para acomodar el subarreglo derecho (menores que el elemento)
-    quicksort(x, pj + 1, ub);//llamada recursiva para acomodar el subarreglo izquierdo (mayores que el elemento)
-}
-
-void generarArchivos(int *arr, int n) { // Cambio de nombre de la función
-    char filename[20]; // Buffer para el nombre del archivo
-    sprintf(filename, "random%d.txt", n); // Genera el nombre del archivo
-    FILE *file = fopen(filename, "w"); // Abre el archivo para escritura
-    srand(time(0)); // Inicializa la semilla del generador de números aleatorios
-    for (int j = 0; j < n; j++) { // Itera n veces
-        arr[j] = rand(); // Genera un número aleatorio y lo guarda en el arreglo
-        fprintf(file, "%d\n", arr[j]); // Escribe el número en el archivo
     }
-    fclose(file); // Cierra el archivo
+    int pj;
+    partition(x, lb, ub, &pj);
+    quicksort(x, lb, pj - 1);
+    quicksort(x, pj + 1, ub);
 }
 
 void partition(int *x, int lb, int ub, int* pj) {
-    int a = *(x+lb);//le asigna a 'a' el elemento en la primera posicon del arreglo o subarreglo
-    int up = ub;//asigna a up el limite superior del arreglo o subarreglo
-    int down = lb;//asigna a lb el limite inferior del arreglo o subarreglo
-    while (down < up) {//mientras que el limite inferior sea menor que el superior
+    int a = *(x+lb);
+    int up = ub;
+    int down = lb;
+    while (down < up) {
         while (*(x+down) <= a && down < ub) {
             down++;
-        }//aumenta down mientras que el elemento en la posicion x[down] sea menor o igual que 'a' y down sea menor que el imite superior del arreglo
+        }
         while (*(x+up) > a) {
             up--;
-        }//decrementa up mientras que el elemnto de la posicion x[up] sea mayor que a;
+        }
         if (down < up) {
             *(x+down) ^= *(x+up);
             *(x+up) ^= *(x+down);
             *(x+down) ^= *(x+up);
-        }//si down es menor que up al final de los while intercambia el valor que tengas en x[down] con x[up]
+        }
     }
-    *(x+lb) = *(x+up);//asigna x[lb] el valor que tengas en x[up]
-    *(x+up) = a; //asignale a x[up] el valor de a
-    *pj = up; //al puntero pj asignale la direccion de up
+    *(x+lb) = *(x+up);
+    *(x+up) = a;
+    *pj = up;
+}
+void llenarArray(int *arr, int longi) {
+    FILE *archivo = fopen("numeros10millones.txt", "r");
+    if (archivo == NULL){
+        printf("No se leyó nada\n");
+        exit(0);
+    }
+    for(int i; i<longi; i++)
+        fscanf(archivo,"%d", &arr[i]);
+
+};
+void generarArchivos(int *arr, int n) {
+    char filename[20];
+    sprintf(filename, "sorted%d.txt", n);
+    FILE *file = fopen(filename, "w");
+    for (int j = 0; j < n; j++) {
+        fprintf(file, "%d\n", arr[j]);
+    }
+    fclose(file);
 }
