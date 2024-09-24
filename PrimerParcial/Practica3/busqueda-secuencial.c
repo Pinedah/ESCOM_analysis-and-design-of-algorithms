06526c5527ddab0f93d0289617ee5d4e493a8556
Andrea Acevedo <aacevedom12@gmail.com>
2024-09-24 14:40:43 -0600

    Secuencial terminado

--- PrimerParcial/Practica3/busqueda-secuencial.c
+++ PrimerParcial/Practica3/busqueda-secuencial.c
@@ -2,63 +2,61 @@
 #include<stdlib.h>
 #include <time.h>
 
-void llenar(int *arr, int longi);
+void llenarArray(int *arr, int longi);
+int buscSec(int *arr, int n, int x);
 
-// An iterative binary search function.
-int binarySearch(int arr[], int low, int high, int x)
-{
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-
-        // Check if x is present at mid
-        if (arr[mid] == x)
-            return mid;
-
-        // If x greater, ignore left half
-        if (arr[mid] < x)
-            low = mid + 1;
-
-        // If x is smaller, ignore right half
-        else
-            high = mid - 1;
-    }
-
-    // If we reach here, then element was not present
-    return -1;
-}
-
-// Driver code
 int main(void)
 {
-    int n = 100;
+    int n = 10000000, x;
     int *arr = malloc(n * sizeof(int));
-    llenar(arr, n);
+    llenarArray(arr, n);
     
     clock_t start, end;
-    int x = 16230884;
+    //Mejor caso
+    //x = arr[0];
+
+    //Caso medio
+    x = arr[n/2];
+
+    //Peor caso
+    //x = -1;
 
     start = clock();  // Inicia el cronómetro antes de la búsqueda
-    int result = binarySearch(arr, 0, n - 1, x);
+    int result = buscSec(arr, n , x);
     end = clock();  // Finaliza el cronómetro después de la búsqueda
-
-     
-    printf("Element is present at index %d\n", result);
-
     double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("Tiempo tomado para buscar %d números: %f segundos\n", n, time_taken);
+    
+    if (result != -1)
+        printf("El elemento buscado esta en el indice %d\n", result);
+    else
+        printf("El elemento no se encontro\n");
+
+    
+    printf("Tiempo tomado para hacer la busqueda en un conjunto de %d numeros: %f segundos\n", n, time_taken);
     free(arr);
 
 }
 
-void llenar(int *arr, int longi) {
-    FILE *archivo = fopen("sorted100.txt", "r");
-    if (archivo == NULL) {
+// Función para leer los datos
+void llenarArray(int *arr, int longi) {
+    char filename[20];
+    sprintf(filename, "sorted%d.txt", longi);
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL){
         printf("No se leyó nada\n");
         exit(0);
     }
-    
-    for(int i = 0; i < longi; i++)  // Inicializa i en 0
-        fscanf(archivo, "%d", &arr[i]);
+    for(int i = 0; i<longi; i++)
+        fscanf(file,"%d", &arr[i]);
+}
 
-    fclose(archivo);  // Asegúrate de cerrar el archivo después de leer
+// Función de la busqueda secuencial
+int buscSec(int *arr, int n, int x){
+    int *ptr;
+    for (ptr=arr; ptr < &arr[n]; ptr++){
+        if (*ptr == x)
+            return ptr - arr;
+    }
+    return -1;
 }
