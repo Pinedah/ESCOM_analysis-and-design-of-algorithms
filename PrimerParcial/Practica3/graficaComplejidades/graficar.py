ce706b755336b1c3aa5d4347dacd6437429c4858
Francisco Pineda <109826677+Pinedah@users.noreply.github.com>
2024-09-25 07:28:33 -0600

    corregir grafica secuencial

--- PrimerParcial/Practica3/graficaComplejidades/graficar.py
+++ PrimerParcial/Practica3/graficaComplejidades/graficar.py
@@ -19,22 +19,22 @@ def guardar_grafica(ruta_guardado, nombre_archivo):
     print(f"Gráfica guardada en: {ruta_completa}")
 
 # Archivos txt con los datos
-binaria = 'binaria.txt' # secuencial
-# casoPromedio = 'casoPromedio.txt'
-secuencial = 'secuencial.txt' # binaria
+mejorCaso = 'mejorCaso.txt' # peorCaso
+casoPromedio = 'casoPromedio.txt'
+peorCaso = 'peorCaso.txt' # mejorCaso
 
 # Leer los datos de los archivos
-x_peor, y_peor = leer_datos(binaria)
-# x_promedio, y_promedio = leer_datos(casoPromedio)
-x_mejor, y_mejor = leer_datos(secuencial)
+x_peor, y_peor = leer_datos(peorCaso)
+x_promedio, y_promedio = leer_datos(casoPromedio)
+x_mejor, y_mejor = leer_datos(mejorCaso)
 
 # Graficar los datos con marcadores más visibles
-plt.plot(x_peor, y_peor, label='Binaria', marker='o', markersize=8, linestyle='-', linewidth=2)
-# plt.plot(x_promedio, y_promedio, label='Caso Promedio', marker='s', markersize=8, linestyle='-', linewidth=2)
-plt.plot(x_mejor, y_mejor, label='Secuencial', marker='^', markersize=8, linestyle='-', linewidth=2)
+plt.plot(x_peor, y_peor, label='Peor Caso', marker='o', markersize=8, linestyle='-', linewidth=2)
+plt.plot(x_promedio, y_promedio, label='Caso Promedio', marker='s', markersize=8, linestyle='-', linewidth=2)
+plt.plot(x_mejor, y_mejor, label='Mejor Caso', marker='^', markersize=8, linestyle='-', linewidth=2)
 
 # Personalización de la gráfica
-plt.title('PEOR CASO')
+plt.title('BUSQUEDA LINEAL')
 plt.xlabel('Cantidad de elementos [n]')
 plt.ylabel('Tiempo [s]')
 plt.legend()  # Mostrar leyendas
@@ -42,4 +42,4 @@ plt.grid(True)  # Mostrar la cuadrícula
 # plt.xscale('log')  # Escala logarítmica en el eje X si fuera necesario
 #vplt.show()
 
-guardar_grafica('graficas', "peorCaso")
+guardar_grafica('graficas', "busqueda-secuencial")
