81d62bbe99440aabc110c25575b1ab9ea8706806
Rodrigo Ramirez <ramirez.aguilar.rodrigo.vidal@gmail.com>
2024-10-22 20:58:07 -0600

    Tiempo para maxmin

--- SegundoParcial/Practica5/arreglos.c
+++ SegundoParcial/Practica5/arreglos.c
@@ -1,19 +1,52 @@
 #include <stdio.h>
+#include <time.h>
+#include<stdlib.h>
 
-void MaxMin_I(int *A, int longi, int *max, int *min){
-    *max = A[0];
-    *min = A[0];
+void llenarArray(int*,int);
+void MaxMin_I(int*, int, int*, int*);
+void MaxMin_R(int*,int,int,int*,int*);
+
+
+
+int main(){
+    int n = 10000000, x;
+    int *arr = malloc(n * sizeof(int));
+    llenarArray(arr, n);
     
-    int *ptr;
-    for(ptr = A; ptr < A + longi; ptr++){
-        if(*ptr > *max)
-            *max = *ptr;
-        
-        if(*ptr < *min)
-            *min = *ptr;
-    }
-}
+    clock_t startI, endI;
+    clock_t startR, endR;
 
+    int ma, mi;
+    int *max;
+    int *min;
+
+    max = &ma;
+    min = &mi;
+    startR = clock();
+    MaxMin_R(arr, 0, n-1, max, min);
+    endR = clock();
+    double time_takenR = ((double)(endR - startR)) / CLOCKS_PER_SEC;
+    printf("Tiempo tomado para hacer la busqueda del maximo y minimo en un conjunto de forma recursiva de %d numeros: %f segundos\n", n, time_takenR);
+    printf("max: %d, min: %d\n", ma, mi);
+    startI = clock();
+    MaxMin_I(arr, n, max, min);
+    endI = clock();
+    double time_takenI = ((double)(endI - startI)) / CLOCKS_PER_SEC;
+    printf("Tiempo tomado para hacer la busqueda del maximo y minimo en un conjunto de forma iterativa de %d numeros: %f segundos\n", n, time_takenI);
+    printf("max: %d, min: %d\n", ma, mi);
+    free(arr);
+    return 0;
+}
+void llenarArray(int *arr, int longi) {
+    FILE *file = fopen("numeros10millones.txt", "r");
+
+    if (file == NULL){
+        printf("No se leyo nada\n");
+        exit(0);
+    }
+    for(int i = 0; i<longi; i++)
+        fscanf(file,"%d", &arr[i]);
+}
 void MaxMin_R(int *A, int i, int j, int *max, int *min){
     if(i < j - 1){
         int mit = (i + j)/2;
@@ -61,21 +94,16 @@ void MaxMin_R(int *A, int i, int j, int *max, int *min){
         *min = *max;
     }
 }
-
-
-int main(){
-
-    int arregloequis[3] = {100,50,70};
-
-    int ma, mi;
-    int *max;
-    int *min;
-
-    max = &ma;
-    min = &mi;
-
-    MaxMin_R(arregloequis, 0, 2, max, min);
-    printf("max: %d, min: %d", ma, mi);
-
-    return 0;
+void MaxMin_I(int *A, int longi, int *max, int *min){
+    *max = A[0];
+    *min = A[0];
+    
+    int *ptr;
+    for(ptr = A; ptr < A + longi; ptr++){
+        if(*ptr > *max)
+            *max = *ptr;
+        
+        if(*ptr < *min)
+            *min = *ptr;
+    }
 }
