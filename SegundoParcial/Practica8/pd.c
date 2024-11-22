64b6ec0cc4a0a828cccfd90b6e5ae3e48886a42c
Andrea Acevedo <aacevedom12@gmail.com>
2024-11-21 21:59:15 -0600

    Pd 1.2 versión

--- SegundoParcial/Practica8/pd.c
+++ SegundoParcial/Practica8/pd.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Función para encontrar el máximo entre dos valores
-int max(int a, int b) {
-    return (a > b) ? a : b;
-}
 
-// Función recursiva para encontrar el máximo beneficio
 int knapSackRec(int W, int wt[], int val[], int index, int** dp, int* selected) {
-    // Condición base
+
     if (index < 0)
         return 0;
 
@@ -19,7 +14,6 @@ int knapSackRec(int W, int wt[], int val[], int index, int** dp, int* selected) 
         // No incluimos este objeto por su peso individual
         dp[index][W] = knapSackRec(W, wt, val, index - 1, dp, selected);
     } else {
-        // Dos opciones: incluir o no incluir el objeto
         int include = val[index] + knapSackRec(W - wt[index], wt, val, index - 1, dp, selected);
         int exclude = knapSackRec(W, wt, val, index - 1, dp, selected);
 
@@ -28,20 +22,21 @@ int knapSackRec(int W, int wt[], int val[], int index, int** dp, int* selected) 
             selected[index] = 1; // Seleccionamos el objeto
         } else {
             dp[index][W] = exclude;
-            selected[index] = 0; // No seleccionamos el objeto
+            selected[index] = 0; // No lo seleccionamos
         }
     }
 
     return dp[index][W];
 }
 
-// Función principal para resolver el problema de la mochila
+
 int knapSack(int W, int wt[], int val[], int n) {
+
     // Declarar la tabla dinámica
     int** dp = (int**)malloc(n * sizeof(int*));
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
         dp[i] = (int*)malloc((W + 1) * sizeof(int));
-    }
+    
 
     // Inicializar la tabla con -1
     for (int i = 0; i < n; i++) 
@@ -53,35 +48,34 @@ int knapSack(int W, int wt[], int val[], int n) {
     for (int i = 0; i < n; i++)
         selected[i] = 0;
 
-    // Resolver el problema
+
     int result = knapSackRec(W, wt, val, n - 1, dp, selected);
 
     // Imprimir resultados
     printf("Peso de mochila disponible: %d\n", W);
     printf("Pesos de objetos(P), Beneficios(B), Seleccion(S)\n");
     printf("P: ");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
         printf("\t%d ", wt[i]);
-    }
+    
     printf("\n");
 
     printf("B: ");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
         printf("\t%d ", val[i]);
-    }
+    
     printf("\n");
 
     printf("S: ");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
         if (selected[i] == 1) {
-            printf("\t%d ", wt[i]); // Imprimir peso del objeto seleccionado
-        } else {
+            printf("\t%d ", wt[i]); 
             printf("\t0 ");
         }
-    }
+    
     printf("\n");
 
-    // Liberar la memoria asignada dinámicamente
+    //Liberar memoria
     for (int i = 0; i < n; i++) 
         free(dp[i]);
     
